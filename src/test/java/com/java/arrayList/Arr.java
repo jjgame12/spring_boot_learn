9f81defc35a9cb977252387dd3197e6074d44627
jjgame12 <360945064@qq.com>
2020-08-12 22:27:24 +0800

    Z123 #comment :art:优化代码

--- src/test/java/com/java/arrayList/Arr.java
+++ src/test/java/com/java/arrayList/Arr.java
@@ -3,6 +3,7 @@ package com.java.arrayList;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Arr {
     @Test
@@ -10,4 +11,22 @@ public class Arr {
         ArrayList<Integer> arr = new ArrayList<>(10);
         System.out.println(arr.size());
     }
+
+    @Test
+    public void test2() {
+        List<A> list = new ArrayList<>();
+        list.add(null);
+        System.out.println(list);
+    }
+
+    @Test
+    public void test3() {
+        Byte a = 1;
+        Byte b = 1;
+        System.out.println(a.toString());
+    }
+}
+
+class A {
+    int id;
 }
